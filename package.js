3526531ada638948ae160e6414db3aa4948055ec
Tomy Ismail <tomysmile@gmail.com>
2015-01-01 19:51:43 +0700

    Importing functions

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'tomysmile:errors',
+  summary: 'A pattern to display application errors to the user',
+  version: '1.0.0',
+  git: 'http://github.com/tomysmile/meteor-errors.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('0.9.0');
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+  api.addFiles(['tomysmile:errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+
+  if (api.export)
+  	api.export('Errors');
+  
+});
+
+Package.onTest(function(api) {
+  api.use('tomysmile:errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');  
+  api.addFiles('tomysmile:errors-tests.js', 'client');
+});
---
+++ tomysmile:errors.js
@@ -0,0 +1,17 @@
+Errors = {
+    // Local (client-only) collection
+    collection: new Mongo.Collection(null),
+
+    throw: function(message) {
+        Errors.collection.insert({
+            message: message,
+            seen: false
+        });
+    },
+
+    clearSeen: function() {
+        Errors.collection.remove({
+            seen: true
+        });
+    }
+};
